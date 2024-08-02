472b0cca8796116d0dba4980be5e1b4e97dff55f
Jose Von Chong <javch1995@gmail.com>
2024-08-02 18:54:13 -0500

    init and first layer

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+
+
+<header class="border-border/40 bg-primary text-primary-foreground w-full border-b backdrop-blur">
+    <div class="container flex h-14 max-w-screen-2xl items-center">
+        <h1 class=" ">Abdelis Barria.</h1>
+        </div>
+
+        
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "$lib/components/Header.svelte";
+</script>
+
+
+
+<body class=" bg-primary text-primary-foreground h-full backdrop-blur-3xl overflow-y-hidden">
+    
+    <div class="container grid mx-auto h-screen grid-cols-1 grid-rows-1 items-center justify-items-center ">
+     
+            <div class="text-center">
+                <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Lic. Abdelis Barria G.
+                  </h1>
+                  <p class="leading-7 [&:not(:first-child)]:mt-6">
+                    Lawyer | Immigration Law | Corporate Law | Intellectual Property | Commercial Law
+                  </p>
+                  <blockquote class="mt-6 border-l-2 pl-6 italic">
+                    "La peor diligencia<br> 
+                    es la que no se hace"
+                  </blockquote>
+                  <p class="leading-7 [&:not(:first-child)]:mt-6">
+                    <a href="mailto:ab@abdelisbarria.com" class=" text-slate-200 font-medium underline underline-offset-4" >ab@abdelisbarria.com</a>
+                  </p>
+
+                
+                  
+            </div>
+       
+
+
+    </div>
+   
+</body>
+
