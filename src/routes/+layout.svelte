f93193067219b24946756a2411567bfee802e3da
Jose Von Chong <javch1995@gmail.com>
2024-08-04 22:18:50 -0500

    adding tag manager container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,50 @@
 	import '../app.css';
 </script>
 
-
+<head>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KWLJGD79');</script>
+	<!-- End Google Tag Manager -->
+</head>
 	
+
+
+<body class=" bg-primary text-primary-foreground h-full backdrop-blur-3xl overflow-y-hidden">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWLJGD79"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 <slot ></slot>
+    <div class="container grid mx-auto h-screen grid-cols-1 grid-rows-1 items-center justify-items-center ">
+     
+            <div class="text-center">
+                <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Lic. Abdelis Barria G.
+                  </h1>
+                  <p class="leading-7 [&:not(:first-child)]:mt-6">
+                    Lawyer | Immigration Law | Corporate Law | Intellectual Property | Commercial Law
+                  </p>
+                  <blockquote class="mt-6 border-l-2 pl-6 italic">
+                    "La peor diligencia<br> 
+                    es la que no se hace"
+                  </blockquote>
+                  <p class="leading-7 [&:not(:first-child)]:mt-6">
+                    <a href="mailto:ab@abdelisbarria.com" class=" text-slate-200 font-medium underline underline-offset-4" >ab@abdelisbarria.com</a>
+                  </p>
+
+                
+                  
+            </div>
+       
+
+
+    </div>
+   
+</body>
+
+
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,32 +4,3 @@
 
 
 
-<body class=" bg-primary text-primary-foreground h-full backdrop-blur-3xl overflow-y-hidden">
-    
-    <div class="container grid mx-auto h-screen grid-cols-1 grid-rows-1 items-center justify-items-center ">
-     
-            <div class="text-center">
-                <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                    Lic. Abdelis Barria G.
-                  </h1>
-                  <p class="leading-7 [&:not(:first-child)]:mt-6">
-                    Lawyer | Immigration Law | Corporate Law | Intellectual Property | Commercial Law
-                  </p>
-                  <blockquote class="mt-6 border-l-2 pl-6 italic">
-                    "La peor diligencia<br> 
-                    es la que no se hace"
-                  </blockquote>
-                  <p class="leading-7 [&:not(:first-child)]:mt-6">
-                    <a href="mailto:ab@abdelisbarria.com" class=" text-slate-200 font-medium underline underline-offset-4" >ab@abdelisbarria.com</a>
-                  </p>
-
-                
-                  
-            </div>
-       
-
-
-    </div>
-   
-</body>
-
